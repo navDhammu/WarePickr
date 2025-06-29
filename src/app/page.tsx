3aca6ce36169b88a0d11816992d88d75b639817f
Navdeep <nsdhammu@gmail.com>
2025-06-29 00:28:47 -0400

    feat: fetch and display pick list in DataTable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
-import Image from 'next/image';
+'use client';
+
+import { useEffect, useState } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { PickList } from './lib/generatePickList';
 
 export default function Home() {
-   return <div>Orders</div>;
+   const [pickList, setPickList] = useState<PickList>([]);
+   const [loading, setLoading] = useState(true);
+
+   useEffect(() => {
+      (async () => {
+         const response = await fetch('/api/picklist');
+         const list = await response.json();
+         setPickList(list);
+         setLoading(false);
+      })();
+   }, []);
+
+   return (
+      <div className='card'>
+         <DataTable
+            value={pickList}
+            loading={loading}
+            tableStyle={{ minWidth: '50rem' }}
+         >
+            <Column field='id' header='Id'></Column>
+            <Column field='name' header='Name'></Column>
+            <Column field='quantity' header='Quantity'></Column>
+         </DataTable>
+         {/* )} */}
+      </div>
+   );
 }
