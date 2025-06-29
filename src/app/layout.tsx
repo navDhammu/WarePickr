c2174ad3b4217ebddb676af61cbd2c02e8ae0d98
Navdeep <nsdhammu@gmail.com>
2025-06-29 15:08:39 -0400

    fix: add missing css import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { PrimeReactProvider } from 'primereact/api';
+import './globals.css';
 import 'primereact/resources/themes/lara-light-cyan/theme.css';
-
 export default function RootLayout({
    children,
 }: Readonly<{
