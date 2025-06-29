c2a0dc7865607dfd8c4c3a996427d6d467f45829
Navdeep <nsdhammu@gmail.com>
2025-06-29 18:00:43 -0400

    feat: add csv export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { PickList } from './lib/generatePickList';
@@ -9,13 +9,18 @@ import { formatDate } from './lib/dateUtils';
 import TableHeader from '@/components/Header';
 import TableFooter from '@/components/Footer';
 import EmptyState from '@/components/EmptyState';
+import { Button } from 'primereact/button';
 
 export default function Home() {
    const [pickList, setPickList] = useState<PickList | null>(null);
    const [loading, setLoading] = useState(true);
    const [date, setDate] = useState(subDays(new Date(), 1));
+   const tableRef = useRef<DataTable<PickList['items']>>(null);
 
-   console.log(pickList);
+   useEffect(() => {
+      tableRef.current?.clearState();
+   }, [pickList]);
+
    const fetchPickList = async (date: Date) => {
       setLoading(true);
       try {
@@ -35,12 +40,32 @@ export default function Home() {
 
    return (
       <div className='p-2 sm:p-4 md:p-6 lg:p-8'>
-         <img src='/WarePickr.png' width={150} className='mb-3' />
+         <div className='flex justify-between items-center mb-5'>
+            <img src='/WarePickr.png' width={150} className='mb-3' />
+            <Button
+               type='button'
+               severity='secondary'
+               rounded
+               outlined
+               size='small'
+               disabled={!pickList?.items?.length}
+               onClick={() => tableRef.current?.exportCSV()}
+            >
+               Export CSV
+            </Button>
+         </div>
          <DataTable
+            ref={tableRef}
             value={pickList?.items}
             emptyMessage={<EmptyState loading={loading} />}
             tableStyle={{ minWidth: '50rem' }}
-            header={<TableHeader date={date} onDateChange={fetchPickList} />}
+            header={
+               <TableHeader
+                  date={date}
+                  onDateChange={fetchPickList}
+                  exportCSV={() => tableRef.current?.exportCSV()}
+               />
+            }
             footer={
                <TableFooter
                   itemsCount={pickList?.itemsCount}
